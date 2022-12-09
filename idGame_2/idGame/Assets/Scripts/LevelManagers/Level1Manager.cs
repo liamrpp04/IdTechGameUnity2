b011585cfde3cb38231094795ec5a82120cbf07b
Ligt04 <liamrpp04@gmail.com>
2022-12-09 11:01:04 -0500

    Monster Changes

--- idGame_2/idGame/Assets/Scripts/LevelManagers/Level1Manager.cs
+++ idGame_2/idGame/Assets/Scripts/LevelManagers/Level1Manager.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using ProjectUtils;
 
 public class Level1Manager : MonoBehaviour
 {
+    public UnityEvent AfterLevel2Load;
     private void Awake()
     {
         Application.targetFrameRate = 60;
     }
-    // Start is called before the first frame update
     void Start()
     {
         this.ActionAfterTime(2f, () =>
@@ -20,14 +21,10 @@ public class Level1Manager : MonoBehaviour
             });
         });
     }
-
+    public void LoadLevel2() => ChangeSceneUI.ChangeSceneAdditive("Level 2", null, () => { AfterLevel2Load.Invoke(); });
     public void OnMonsterAppear()
     {
-        ObjectivesUI.CompleteObjective("1", () =>
-        {
-            ObjectivesUI.AddObjective("2", "Escape the beast");
-        });
-
+        ObjectivesUI.CompleteObjective("1", () =>{ObjectivesUI.AddObjective("2", "Escape the beast");});
     }
 
 }
---
+++ idGame_2/idGame/Assets/Scripts/UIScripts/ChangeSceneUI.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System;
+
+public class ChangeSceneUI : MonoBehaviour
+{
+    #region Private vals
+    private static ChangeSceneUI Instance;
+    #endregion
+    #region SerializeFielded vals
+    [SerializeField] private Image fadeImage;
+    #endregion
+    #region Public vals
+
+    #endregion
+
+
+
+    public static void ChangeScene(string sceneName, Action OnCompleteFade = null) => Instance._ChangeScene(sceneName, OnCompleteFade);
+    public static void ChangeScene(string sceneName) => Instance._ChangeScene(sceneName);
+    public static void ChangeSceneAdditive(string sceneName, Action OnCompleteFade = null, Action afterSceneLoad = null) => Instance._ChangeSceneAdditive(sceneName, OnCompleteFade,afterSceneLoad);
+    public static void ChangeSceneAdditive(string sceneName) => Instance._ChangeSceneAdditive(sceneName);
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LetShowScene();
+        SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) => LetShowScene();
+    }
+
+    private void LetShowScene()
+    {
+        Time.timeScale = 1;
+        fadeImage.color = Color.black;
+        fadeImage.DOFade(0, 1f).SetDelay(0.1f).OnComplete(() =>
+        {
+
+        });
+    }
+
+    private void _ChangeScene(string sceneName, Action OnCompleteFade = null)
+    {
+        fadeImage.DOFade(1, 1f).SetUpdate(true).OnComplete(() =>
+        {
+            OnCompleteFade?.Invoke();
+            SceneManager.LoadScene(sceneName);
+        });
+    }
+
+    private void _ChangeSceneAdditive(string sceneName, Action OnCompleteFade = null, Action afterSceneLoad = null)
+    {
+        fadeImage.DOFade(1, 1f).SetUpdate(true).OnComplete(() =>
+        {
+            OnCompleteFade?.Invoke();
+            SceneManager.LoadScene(sceneName,LoadSceneMode.Additive);
+            afterSceneLoad?.Invoke();
+        });
+    }
+}
